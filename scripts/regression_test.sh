0e25d334eac737f04347f2f76a07594493d6cc91
Marc Foley <m.foley.88@gmail.com>
2026-05-05 15:43:38 +0100

    refactor to use diffenator3

--- scripts/regression_test.sh
+++ scripts/regression_test.sh
@@ -11,27 +11,4 @@ DL_URL=$(curl https://api.github.com/repositories/86081751/releases/latest | jq 
 wget $DL_URL
 unzip Roboto_*.zip -d prev_release
 
-
-# Diff old hinted variable font against current
-diff ()
-{
-    diffenator $OLD_FONT $GENNED_FONT -i "$2" \
-        -html > $1/index.html \
-        -rd -r ./img/ \
-        --ft-hinting normal
-    mv img/ $1
-}
-
-
-mkdir -p diffs \
-	 diffs/Regular \
-	 diffs/Condensed \
-	 diffs/Italic \
-	 diffs/CondensedItalic
-
-
-diff diffs/Regular "wght=400, wdth=100"
-diff diffs/Condensed "wght=400, wdth=75"
-diff diffs/Italic "ital=1, wght=400, wdth=100"
-diff diffs/CondensedItalic "ital=1, wght=400, wdth=75"
-
+diffenator3 $OLD_FONT $GENNED_FONT --html --output diffs
